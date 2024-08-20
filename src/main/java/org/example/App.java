117f931aad7c8fea3be4a22052051332032f616f
hyeon924 <hyeonn4713@gmail.com>
2024-08-20 13:18:19 +0900

    refactoring

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,14 +6,13 @@ import java.util.Scanner;
 
 public class App {
     Scanner sc;
-
+    List<Article> articleList = new ArrayList<>();
     App (Scanner sc) {
         this.sc = sc;
     }
 
     void run () {
         int lastId = 1;
-        List<Article> articleList = new ArrayList<>();
         System.out.println("== 게시판 앱 ==");
         while (true) {
             System.out.print("명령) ");
@@ -51,12 +50,7 @@ public class App {
                 String value = paramsStr[1];
                 int idx = Integer.parseInt(value);
 
-                Article article = null;
-                for (int i = 0; i < articleList.size(); i++) {
-                    if (articleList.get(i).getId() == idx) {
-                        article = articleList.get(i);
-                    }
-                }
+                Article article = _getFindById(idx);
 
                 if(article == null) {
                     System.out.printf("%d번 게시물은 존재하지 않습니다.\n", idx);
@@ -75,12 +69,9 @@ public class App {
                 String value = paramsStr[1];
                 int idx = Integer.parseInt(value);
 
-                Article article = null;
-                for (Article item : articleList) {
-                    if(item.getId() == idx) {
-                        article = item;
-                    }
-                }
+
+                Article article = _getFindById(idx);
+
                 if(article == null) {
                     System.out.printf("%d번 게시물은 존재하지 않습니다.\n", idx);
                 } else {
@@ -99,5 +90,14 @@ public class App {
 
             }
         }
+    }
+
+    private Article _getFindById(int id) {
+        for (Article item : articleList) {
+            if(item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
     }
 }

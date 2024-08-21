fa1ae6351352745684a37e46e0f0aea32412f6a4
hyeon924 <hyeonn4713@gmail.com>
2024-08-21 10:48:56 +0900

    refactor

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,103 +1,40 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import org.example.article.ArticleController;
+import org.example.system.SystemController;
 
 public class App {
-    Scanner sc;
-    List<Article> articleList = new ArrayList<>();
-    App (Scanner sc) {
-        this.sc = sc;
+    ArticleController articleController;
+    SystemController systemController;
+
+
+    App () {
+        articleController = new ArticleController();
+        systemController = new SystemController();
     }
 
-    void run () {
-        int lastId = 1;
+    public void run () {
         System.out.println("== 게시판 앱 ==");
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine().trim();
+            String command = Container.getSc().nextLine().trim();
+            // 커맨드에 입력한 내용을 actionCode, idx로 분류해서 필드로 저장
+            Request request = new Request(command);
 
-            if (command.equals("end")) {
+            if (request.getActionCode().equals("end")) {
+                systemController.exit();
                 break;
-            } else if (command.equals("add")) { //equals 문자열을 정확하게 비교하기 위해서 작성
-                System.out.print("subject : ");
-                String subject = sc.nextLine().trim(); //trim 공백을 제거하고 받기 위해서 작성
-                System.out.print("content : ");
-                String content = sc.nextLine().trim();
-
-
-                Article article = new Article(lastId, subject, content);
-                articleList.add(article);
-
-                System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId); // 증감 처리, 게시물이 등록 될 때마다 n번째로 증감이 되어야함.
-                lastId++;
-            } else if (command.equals("list")) {
-                System.out.println("Number / Title / Content");
-                System.out.println("----------------------");
-                for (int i = 0; i < articleList.size(); i++) { //int i = articleList.size() - 1; i >= 0; i-- 으로 조건을 바꿔주면 목록이 반대로 출력됨
-                    Article article = articleList.get(i);
-                    System.out.printf("%d, %s, %s\n", article.getId(), article.getSubject(), article.getContent());
-                }
-            } else if (command.startsWith("delete")) {
-                String[] commandList = command.split("\\?", 2);
-
-                String actionCode = commandList[0];
-
-                String[] paramsStr = commandList[1].split("=", 2);
-
-                String key = paramsStr[0];
-                String value = paramsStr[1];
-                int idx = Integer.parseInt(value);
-
-                Article article = _getFindById(idx);
-
-                if(article == null) {
-                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", idx);
-                } else {
-                    articleList.remove(article);
-                    System.out.printf("%d번 게시물이 삭제되었습니다.\n", idx);
-                }
-            } else if (command.startsWith("update")) {
-                String[] commandList = command.split("\\?", 2);
-
-                String actionCode = commandList[0];
-
-                String[] paramsStr = commandList[1].split("=", 2);
-
-                String key = paramsStr[0];
-                String value = paramsStr[1];
-                int idx = Integer.parseInt(value);
-
-
-                Article article = _getFindById(idx);
-
-                if(article == null) {
-                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", idx);
-                } else {
-                    System.out.printf("제목(기존) : %s\n", article.getSubject());
-                    System.out.print("제목 : ");
-                    String modifySubject = sc.nextLine();
-                    article.setSubject(modifySubject);
-
-                    System.out.printf("내용(기존) : %s\n", article.getContent());
-                    System.out.print("내용 : ");
-                    String modifyContent = sc.nextLine();
-                    article.setContent(modifyContent);
-
-                    System.out.printf("%d번 게시물이 수정되었습니다.\n", idx);
-                }
-
+            } else if (request.getActionCode().equals("add")) {
+                articleController.write();
+            } else if (request.getActionCode().equals("list")) {
+                articleController.list();
+            } else if (request.getActionCode().startsWith("delete")) {
+                articleController.delete(request.getIdx());
+            } else if (request.getActionCode().startsWith("modify")) {
+                articleController.modify(request.getIdx());
             }
         }
     }
 
-    private Article _getFindById(int id) {
-        for (Article item : articleList) {
-            if(item.getId() == id) {
-                return item;
-            }
-        }
-        return null;
-    }
+
 }
---
+++ src/main/java/org/example/article/ArticleController.java
@@ -0,0 +1,75 @@
+package org.example.article;
+
+import org.example.Container;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleController {
+    List<Article> articleList = new ArrayList<>();
+    int lastId = 1;
+
+    public void write() {
+        System.out.print("제목 : ");
+        String subject = Container.getSc().nextLine().trim();
+        System.out.print("내용 : ");
+        String content = Container.getSc().nextLine().trim();
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId);
+
+        Article article = new Article(lastId, subject, content);
+        articleList.add(article);
+
+        lastId++;
+    }
+
+    public void list () {
+        System.out.println("번호 / 제목 / 내용");
+        System.out.println("----------------------");
+        for (int i = articleList.size() - 1; i >= 0; i--) {
+            Article article = articleList.get(i);
+            System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
+        }
+
+    }
+    public void delete (int idx) {
+        Article article = _getFindById(idx);
+
+        if (article == null) {
+            System.out.printf("%d번 게시물은 존재하지 않습니다.\n", idx);
+        } else {
+            articleList.remove(article);
+            System.out.printf("%d번 게시물이 삭제되었습니다.\n", idx);
+        }
+    }
+    public void modify (int idx) {
+        Article article = _getFindById(idx);
+
+        if (article == null) {
+            System.out.printf("%d번 게시물은 존재하지 않습니다.\n", idx);
+        } else {
+
+            System.out.printf("제목(기존) : %s\n", article.getSubject());
+            System.out.print("제목 : ");
+            String modifySubject = Container.getSc().nextLine();
+            article.setSubject(modifySubject);
+
+            System.out.printf("내용(기존) : %s\n", article.getContent());
+            System.out.print("내용 : ");
+            String modifyContent = Container.getSc().nextLine();
+            article.setContent(modifyContent);
+
+            System.out.printf("%d번 게시물이 수정되었습니다.\n", idx);
+        }
+    }
+
+
+    private Article _getFindById(int id) {
+        for (Article item : articleList) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+
+        return null;
+    }
+}
